57144226facad5097293c9dc2a1e3b014a4bfef2
xgrg <goperto@gmail.com>
2021-04-30 21:03:18 +0200

    test

--- roistats/plotting.py
+++ roistats/plotting.py
@@ -1,28 +1,29 @@
+from matplotlib import pyplot as plt
 import seaborn as sns
 import pandas as pd
 import logging as log
 from .__init__ import correct
+plt.style.use('classic')
+plt.style.use('seaborn')
 
 
 default_palette = {
-    0: '#99ccff',
-    'HO':'#ff9999',
-    'HT':'#ffd699',
-    'NC':'#99ccff',
-    'not HO':'#99ccff',
-    'female':'#ff9999',
-    'male':'#99ccff',
-    'apoe44':'#ff9999',
-    'apoe34':'#ffd699',
-    'apoe33':'#99ccff',
+    'HO': '#ff9999',
+    'HT': '#ffd699',
+    'NC': '#99ccff',
+    'not HO': '#99ccff',
+    'female': '#ff9999',
+    'male': '#99ccff',
+    'apoe44': '#ff9999',
+    'apoe34': '#ffd699',
+    'apoe33': '#99ccff',
     'carriers': '#ffd699',
-    'AnTn':'#6495ed',
-    'ApTn':'#544a8c',
-    'ApTp':'#850d04',
-    True:'#c24d4a',
-    False: '#6495ed'
+    'AnTn': '#6495ed',
+    'ApTn': '#ff962c',
+    'ApTp': '#850d04',
+    True: '#c24d4a',
+    False: '#6495ed'}
 
-    }
 
 def ttest_2samp(y, data, x1, x2, by, covariates, groups=None):
     import logging as log
@@ -30,24 +31,25 @@ def ttest_2samp(y, data, x1, x2, by, covariates, groups=None):
 
     data_dummies = pd.get_dummies(data, columns=[by])
     if groups is None:
-        groups = {x1:[x1], x2:[x2]}
+        groups = {x1: [x1], x2: [x2]}
 
     dummy_columns = []
     for x in [x1, x2]:
         for each in groups[x]:
-            dummy_columns.append('%s_%s'%(by, each))
+            dummy_columns.append('%s_%s' % (by, each))
 
-    formula = '%s ~ %s%s + 1'%(y, ' + '.join(dummy_columns),
-          {False:'', True: ' + %s'%' + '.join(covariates)}[len(covariates)!=0])
-    log.warning('Used model for significance estimation: %s'%formula)
+    formula = '%s ~ %s%s + 1' % (y, ' + '.join(dummy_columns),
+                                 {False: '', True: ' + %s' % ' + '.join(covariates)}[len(covariates) != 0])
+    log.warning('Used model for significance estimation: %s' % formula)
     fitted_model = ols(formula, data_dummies).fit()
-    s1 = ['%s * %s_%s'%(1.0/len(groups[x1]), by, each) for each in groups[x1]]
-    s2 = ['%s * %s_%s'%(1.0/len(groups[x2]), by, each) for each in groups[x2]]
+    s1 = ['%s * %s_%s' % (1.0/len(groups[x1]), by, each) for each in groups[x1]]
+    s2 = ['%s * %s_%s' % (1.0/len(groups[x2]), by, each) for each in groups[x2]]
 
-    c = '%s - %s'%(' + '.join(s1), ' - '.join(s2))
+    c = '%s - %s' % (' + '.join(s1), ' - '.join(s2))
     T = fitted_model.t_test(c)
-    log.warning('Used contrast: %s - p-value: %s'%(c, T.pvalue))
+    log.warning('Used contrast: %s - p-value: %s' % (c, T.pvalue))
     return T.pvalue
+
 
 def _plot_significance(data, x1, x2, by, covariates, groups):
     from matplotlib import pyplot as plt
@@ -64,22 +66,22 @@ def _plot_significance(data, x1, x2, by, covariates, groups):
     # shift the bar in y to avoid overlap between tests
     y = y + 4 * (i2 - i1 - 1) * h
     plt.plot([i1, i1, i2, i2], [y, y+h, y+h, y], lw=1.5, c=col)
-    opt = {'ha':'center', 'va':'bottom', 'color':col}
+    opt = {'ha': 'center', 'va': 'bottom', 'color': col}
     pval = ttest_2samp('y', data, x1, x2, by, covariates, groups)
 
     if pval < 0.05:
         opt['weight'] = 'bold'
-    plt.text((i1+i2)*.5, y + h, '%.3f'%(pval), **opt)
+    plt.text((i1+i2)*.5, y + h, '%.3f' % (pval), **opt)
 
     return pval
 
+
 def sort_groups(data, by, order):
     # Create a column with a group index based on `groups`
-    col = []
     d = pd.DataFrame(data, copy=True)
-    d[by] = d[by].replace({group:order.index(group) for group in order})
-    #d = d.sort_values(by=by)
-    d[by] = d[by].replace({order.index(group):group for group in order})
+    d[by] = d[by].replace({group: order.index(group) for group in order})
+    # d = d.sort_values(by=by)
+    d[by] = d[by].replace({order.index(group): group for group in order})
     return d
 
 
@@ -90,7 +92,7 @@ def boxplot(y, data, by='apoe', covariates=[], palette=None, groups=None):
 
     from collections import OrderedDict
 
-    if palette == 'apoe':
+    if palette == 'default':
         palette = default_palette
 
     if groups is None:
@@ -101,7 +103,7 @@ def boxplot(y, data, by='apoe', covariates=[], palette=None, groups=None):
     for k, each in groups.items():
         all_groups.extend(each)
     if set(all_groups).difference(set(data[by].tolist())):
-        raise Exception('%s not all found as %s groups'%(all_groups, by))
+        raise Exception('%s not all found as %s groups' % (all_groups, by))
 
     # Create a column with a group index based on `groups`
     col = []
@@ -119,30 +121,31 @@ def boxplot(y, data, by='apoe', covariates=[], palette=None, groups=None):
 
     # check that all variables are found in columns
     for e in variables:
-        if not e in d.columns.tolist():
-            raise Exception('%s not found in columns!'%e)
+        if e not in d.columns.tolist():
+            raise Exception('%s not found in columns!' % e)
 
-    df = pd.DataFrame(d, columns=list(variables)).rename(columns={y:'y'})
+    df = pd.DataFrame(d, columns=list(variables)).rename(columns={y: 'y'})
     df = df.dropna()
     y = 'y'
-    log.info('Dependent variable: %s'%roi_name)
+    log.info('Dependent variable: %s' % roi_name)
+    print(list(groups.keys()))
     df = sort_groups(df, by, list(groups.keys()))
 
     # Correct depending variable for covariates (if any) (df is modified)
     if len(covariates) != 0:
-        df[y]  = correct(df, '%s ~ %s  + 1'%(y, '+'.join(covariates)))
+        df[y] = correct(df, '%s ~ %s  + 1' % (y, '+'.join(covariates)))
 
     box = sns.boxplot(x='_group', y='y', data=df, showfliers=False,
-        palette=palette)
-    box = sns.swarmplot(x='_group', y='y', data=df, #palette=palette,
-        edgecolor='gray', color='0.8', linewidth=1)
-    box.axes.set_yticklabels(['%.2e'%x for x in box.axes.get_yticks()])
+                      palette=palette, order=list(groups.keys()))
+    box = sns.swarmplot(x='_group', y='y', data=df,  # palette=palette,
+                        edgecolor='gray', color='0.8', linewidth=1,
+                        order=list(groups.keys()))
+    box.axes.set_yticklabels(['%.2e' % x for x in box.axes.get_yticks()])
 
-    box.axes.set_xlabel('%s groups'%by, fontsize=15, weight='bold')
-    ylabel = '%s'\
-                %{False:'',
-                  True:' (corrected for %s)'\
-                     %(' and '.join(covariates))}[len(covariates)!=0]
+    box.axes.set_xlabel('%s groups' % by, fontsize=15, weight='bold')
+    ylabel = '%s' % {False: '',
+                     True: ' (corrected for %s)'
+                           % (' and '.join(covariates))}[len(covariates) != 0]
     box.axes.set_ylabel(ylabel)
     box.set_title(roi_name)
     #box.axes.set_ylim(1.5,4)
@@ -153,87 +156,84 @@ def boxplot(y, data, by='apoe', covariates=[], palette=None, groups=None):
 
     for i1, i2 in itertools.combinations(groups.keys(), 2):
         pval = _plot_significance(df, i1, i2, by, covariates, groups)
-        pvals.append((pval, (i1,i2)))
+        pvals.append((pval, (i1, i2)))
 
     return pvals
 
 
 def lmplot(y, x, data, covariates=['gender', 'age'], hue='apoe', ylim=None,
-        savefig=None, facecolor='white', order=1, palette=None, size=None,
-	s=20):
+           savefig=None, facecolor='white', order=1, palette=None, size=None,
+           s=20, lwl=2, lws=1, ylabel=''):
 
-    from matplotlib import pyplot as plt
-    import matplotlib as mpl
     plt.style.use('classic')
     plt.style.use('seaborn')
 
-    if palette is 'apoe':
+    if palette == 'default':
         palette = default_palette
 
     # Build a new table with only needed variables
     # y is renamed to roi to avoid potential issues with strange characters
     roi_name = y
-    log.info('Dependent variable: %s'%y)
+    log.info('Dependent variable: %s' % y)
     variables = {x, y}
-    if not hue is None:
+    if hue is not None:
         variables.add(hue)
     for c in covariates:
         variables.add(c)
 
     # check that all variables are found in columns
     for e in variables:
-        if not e in data.columns.tolist():
-            raise Exception('%s not found in columns!'%e)
+        if e not in data.columns.tolist():
+            raise Exception('%s not found in columns!' % e)
 
-    df = pd.DataFrame(data, columns=list(variables)).rename(columns={y:'y'})
+    df = pd.DataFrame(data, columns=list(variables)).rename(columns={y: 'y'})
     df = df.dropna()
     y = 'y'
 
     # Correct depending variable for covariates (if any) (df is modified)
     if len(covariates) != 0:
-        df[y]  = correct(df, '%s ~ %s  + 1'%(y, '+'.join(covariates)))
-    #    hue_order=[ 'Whole_hippocampus',
-    #    'Hippocampal_tail', 'molecular_layer_HP', 'CA1',
-    #    'presubiculum', 'subiculum',
-    #    'GC_ML_DG', 'CA4', 'fimbria','CA3', 'HATA',
-    #    'hippocampal_fissure', 'parasubiculum'],
-    lm = sns.lmplot(x=x, y=y,  data=df, height=6.2, hue=hue, size=size, aspect=1.35,
-         ci=95,
-         truncate=True, sharex=False, sharey=False, order=order, palette=palette,
-         scatter_kws={'linewidths':0.5, 's':s, 'edgecolors':'#333333' },
-         line_kws={'linewidth':1}, legend=True)
+        df[y] = correct(df, '%s ~ %s  + 1' % (y, '+'.join(covariates)))
 
-    for patch in lm.axes[0,0].patches:
+    lm = sns.lmplot(x=x, y=y,  data=df, height=6.2, hue=hue, size=size,
+                    aspect=1.35, ci=95, truncate=True, sharex=False,
+                    sharey=False, order=order, palette=palette,
+                    scatter_kws={'linewidths': lws, 's': s,
+                                 'edgecolors': '#333333'},
+                    line_kws={'linewidth': lwl}, legend=True)
+
+    for patch in lm.axes[0, 0].patches:
         clr = patch.get_facecolor()
-        patch.set_edgecolor(palette2[clr])
+        patch.set_edgecolor(palette[clr])
     ax = lm.axes
     if ylim is None:
-        ax[0,0].set_ylim([df[y].min(), df[y].max()])
+        ax[0, 0].set_ylim([df[y].min(), df[y].max()])
     else:
-        ax[0,0].set_ylim(ylim)
-    ax[0,0].set_xlim([df[x].min(), df[x].max()])
-    #ax[0,0].set_yticklabels(['%.2e'%i for i in ax[0,0].get_yticks()])
-    #ax[0,0].set_yticklabels(['%.2e'%i for i in ax[0,0].get_yticks()])
-    ax[0,0].tick_params(labelsize=12)
-    ax[0,0].set_ylabel('%s'%({False:'',
-        True:' (corrected for %s)'\
-            %(' and '.join(covariates))}[len(covariates)!=0]), fontsize=13)
-    ax[0,0].set_xlabel(x, fontsize=15, weight='bold')
+        ax[0, 0].set_ylim(ylim)
+    ax[0, 0].set_xlim([df[x].min(), df[x].max()])
+    # ax[0,0].set_yticklabels(['%.2e'%i for i in ax[0,0].get_yticks()])
+    # ax[0,0].set_yticklabels(['%.2e'%i for i in ax[0,0].get_yticks()])
+    ax[0, 0].tick_params(labelsize=12)
+    ax[0, 0].set_ylabel('%s%s' % (ylabel, {False: '',
+                                           True: ' (corrected for %s)'
+            % (' and '.join(covariates))}[len(covariates) != 0]), fontsize=13)
+    ax[0, 0].set_xlabel(x, fontsize=15, weight='bold')
     lm.fig.suptitle(roi_name, fontsize=15)
 
-    if not savefig is None:
+    if savefig is not None:
         lm.savefig(savefig, facecolor=facecolor)
     return df
 
 
-def _pivot(data, covariates=[], regions=None, region_colname='region', value_colname='value'):
+def _pivot(data, covariates=[], regions=None, region_colname='region',
+           value_colname='value'):
 
     #print regions
     index_colname = '_ID'
     if regions is None:
         regions = set(data[region_colname].tolist())
     data2 = data[data[region_colname].isin(regions)]
-    piv = pd.pivot_table(data2, values=value_colname, index=data2.index, columns=region_colname)
+    piv = pd.pivot_table(data2, values=value_colname, index=data2.index,
+                         columns=region_colname)
     subject_list = list(data2.index.tolist())
     data2[index_colname] = subject_list
     columns = [index_colname]
@@ -242,6 +242,7 @@ def _pivot(data, covariates=[], regions=None, region_colname='region', value_col
     piv = piv.join(cov)
 
     return piv
+
 
 def _unpivot(piv, regions, region_colname='structure', value_colname='value'):
     table = []
@@ -254,18 +255,18 @@ def _unpivot(piv, regions, region_colname='structure', value_colname='value'):
     data = pd.DataFrame(table, columns=columns).set_index(index_colname)
     return data
 
+
 def hist(data, regions=None, by=None, covariates=[], palette=None,
-    zscores=False, ylim=None, region_colname='structure', value_colname='value',
-    hue_order=None, **kwargs):
-    from matplotlib import pyplot as plt
+         zscores=False, ylim=None, region_colname='structure',
+         value_colname='value', hue_order=None, **kwargs):
 
     # check that all variables are found in columns
     # and if so collect covariables
     columns = [] if by is None else [by]
     columns.extend(covariates)
     for e in columns:
-        if not e in data.columns.tolist():
-            raise Exception('%s not found in columns!'%e)
+        if e not in data.columns.tolist():
+            raise Exception('%s not found in columns!' % e)
     cov = pd.DataFrame(data, columns=columns)
 
     # first pivot region/value entries to individual columns
@@ -279,18 +280,19 @@ def hist(data, regions=None, by=None, covariates=[], palette=None,
 
     for region in regions:
         if len(covariates) != 0:
-            if not region in piv.columns.tolist():
-                raise Exception('%s not found in regions! (%s)'%(region, regions))
-            piv = piv.rename(columns={region:'y'})
-            piv['y'] = correct(piv, '%s ~ %s  + 1'%('y', '+'.join(covariates)))
+            if region not in piv.columns.tolist():
+                msg = '%s not found in regions! (%s)' % (region, regions)
+                raise Exception(msg)
+            piv = piv.rename(columns={region: 'y'})
+            piv['y'] = correct(piv, '%s ~ %s  + 1' % ('y', '+'.join(covariates)))
 
-            if not by is None:
+            if by is not None:
                 groups = list(set(piv[by].tolist()))
                 for i1, i2 in itertools.combinations(groups, 2):
                     pval = ttest_2samp('y', piv, i1, i2, by, covariates)
                     pvals.append((region, (i1, i2), pval))
-            piv = piv.rename(columns={'y':region})
-        if zscores: # convert them to zscores if asked
+            piv = piv.rename(columns={'y': region})
+        if zscores:  # convert them to zscores if asked
             m = piv[region].mean()
             s = piv[region].std()
             piv[region] = (piv[region] - m)/s
@@ -306,26 +308,27 @@ def hist(data, regions=None, by=None, covariates=[], palette=None,
         col.append(regions.index(row[region_colname]))
     data2['rank'] = col
 
-    if palette is None:
+    if palette == 'default':
         palette = default_palette
         if by is None:
             data2['_by'] = 'NC'
             by = '_by'
 
     # plot
-    plt.figure(figsize=(30,12))
-    bar = sns.catplot(x='rank', y =value_colname, hue=by, data=data2,
-        palette=palette, errwidth=2, ci=90, legend=not by=='_by', kind='bar',
-        hue_order=hue_order, capsize=.05, **kwargs)
+    plt.figure(figsize=(30, 12))
+    bar = sns.catplot(x='rank', y=value_colname, hue=by, data=data2,
+                      palette=palette, errwidth=2, ci=90,
+                      legend=not by == '_by', kind='bar',
+                      hue_order=hue_order, capsize=.05, **kwargs)
 
     bar.set_xticklabels(rotation=45)
     bar.set_xticklabels(regions)
 
-    if not ylim is None:
+    if ylim is not None:
         plt.ylim(ylim)
-    ylabel = '%s %s'\
-                %(value_colname, {False:'',
-                  True:' (corrected for %s)'\
-                     %(' and '.join(covariates))}[len(covariates)!=0])
+
+    ylabel = '%s %s' % (value_colname, {False: '',
+                                        True: ' (corrected for %s)'
+                                              % (' and '.join(covariates))}[len(covariates) != 0])
     bar.set(xlabel=region_colname, ylabel=ylabel)
     return pvals
