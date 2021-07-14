555ddc75606601de075c8be909f4813b47a34172
xgrg <goperto@gmail.com>
2021-07-14 12:19:03 +0200

    code linting

--- roistats/atlases/__init__.py
+++ roistats/atlases/__init__.py
@@ -2,16 +2,17 @@ import os
 import os.path as op
 from glob import glob
 
-def labels(fn=None): #'JHU-tracts.xml'):
+
+def labels(fn=None):  #'JHU-tracts.xml'):
     fsldir = os.environ.get('FSLDIR')
     if fsldir is None:
         raise Exception('FSLDIR not found.')
 
     atlasdir = op.join(fsldir, 'data', 'atlases')
     if fn is None:
-        print('Available atlases (%s):\n'%atlasdir)
-        print(' - %s'%'\n - '.join([op.basename(e) for e in \
-            glob(op.join(atlasdir, '*.xml'))]))
+        print('Available atlases (%s):\n' % atlasdir)
+        print(' - %s' % '\n - '.join([op.basename(e) for e in
+                                      glob(op.join(atlasdir, '*.xml'))]))
     else:
         x = open(op.join(atlasdir, fn)).read()
         a = 0
@@ -33,28 +34,29 @@ def labels(fn=None): #'JHU-tracts.xml'):
                 for e in x.split('\n') if e.startswith('<label')]))
         return d
 
+
 def maps(name=None, fn=None):
     fsldir = os.environ.get('FSLDIR')
     if fsldir is None:
         raise Exception('FSLDIR not found.')
 
     atlasdir = op.join(fsldir, 'data', 'atlases')
-    dirs = [op.basename(e) for e in glob(op.join(atlasdir, '*')) \
-        if op.isdir(op.join(atlasdir, e)) and op.basename(e) != 'bin']
-    if not name is None and not name in dirs:
-        print('%s not found'%name)
+    dirs = [op.basename(e) for e in glob(op.join(atlasdir, '*'))
+            if op.isdir(op.join(atlasdir, e)) and op.basename(e) != 'bin']
+    if name is not None and name not in dirs:
+        print('%s not found ' % name)
     if name is None or name not in dirs:
-        print('Available atlases (%s):\n'%atlasdir)
-        print(' - %s'%'\n - '.join(sorted(dirs)))
+        print('Available atlases (%s):\n' % atlasdir)
+        print(' - %s' % '\n - '.join(sorted(dirs)))
     else:
         files = [op.basename(e) for e in glob(op.join(atlasdir, name, '*'))]
-        if not fn is None and not fn in files:
-            print('%s not found'%fn)
+        if fn is not None and fn not in files:
+            print('%s not found' % fn)
         if fn is None or fn not in files:
-            print('Available version in %s:\n'%name)
-            print(' - %s'%'\n - '.join(sorted(files)))
+            print('Available version in %s:\n' % name)
+            print(' - %s' % '\n - '.join(sorted(files)))
         else:
-            print('Loading atlas %s/%s'%(name, fn))
+            print('Loading atlas %s/%s' % (name, fn))
             import nibabel as nib
             fp = op.join(atlasdir, name, fn)
             return nib.load(fp)
