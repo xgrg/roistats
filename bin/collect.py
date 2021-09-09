e2d42e1149981345df7f1c05e08216b55238ed9b
xgrg <goperto@gmail.com>
2021-09-09 18:18:29 +0200

    ENH: if JHU rename columns

--- bin/collect.py
+++ bin/collect.py
@@ -5,6 +5,8 @@ import numpy as np
 from roistats import collect
 import json
 import logging as log
+import os
+import os.path as op
 
 
 desc = 'Collect ROI stats over a set of images and store them in an Excel '\
@@ -15,9 +17,9 @@ if __name__ == '__main__':
     parser.add_argument('images', nargs='+', help='Input images')
     parser.add_argument('-o', '--output', required=True,
                         help='Excel file to store the output')
-    parser.add_argument('--roi', required=True,
-                        help='Atlas (or any label volume) containing the '\
-                             'reference regions')
+    help = 'Atlas (or any label volume) containing the '\
+         'reference regions (supported atlases: JHU-labels, JHU-tracts)'
+    parser.add_argument('--atlas', required=True, help=help)
     parser.add_argument('--n_jobs', default=-1,
                         help='Number of parallel jobs')
     parser.add_argument('--function', default='mean',
@@ -30,9 +32,41 @@ if __name__ == '__main__':
         log.basicConfig(level=log.INFO)
 
     n_jobs = int(opts.n_jobs)
-    table = collect.roistats_from_maps(opts.images, opts.roi, opts.images,
+
+    from roistats import atlases
+    fsldir = os.environ.get('FSLDIR')
+    if fsldir is None:
+        raise Exception('FSLDIR not found.')
+    atlasdir = op.join(fsldir, 'data', 'atlases')
+
+    if not opts.atlas in ['JHU-labels', 'JHU-tracts']:
+        atlas = op.abspath(opts.atlas)
+        if not op.isfile(atlas):
+            msg = '%s is not an existing image nor a supported atlas (JHU-lab'\
+                  'els/JHU-tracts)' % atlas
+            raise FileNotError(msg)
+
+
+    elif opts.atlas == 'JHU-labels':
+        labels = atlases.labels('JHU-labels.xml')
+        fn = 'JHU-ICBM-labels-1mm.nii.gz'
+        atlas = op.join(atlasdir, 'JHU', fn)
+
+    elif opts.atlas == 'JHU-tracts':
+        labels = atlases.labels('JHU-tracts.xml')
+        fn = 'JHU-ICBM-tracts-maxprob-thr0-1mm.nii.gz'
+        atlas = op.join(atlasdir, 'JHU', fn)
+
+    table = collect.roistats_from_maps(opts.images, atlas, opts.images,
                                        getattr(np, opts.function),
                                        n_jobs)
     table.to_excel(opts.output)
-    d = {'images': opts.images, 'atlas': opts.roi}
-    json.dump(d, open(opts.output.replace('.xls', '.json'), 'w'))
+
+    # rename columns
+    if opts.atlas in ['JHU-labels', 'JHU-tracts']:
+        table = table.rename(columns=labels)
+        table.to_excel(opts.output)
+
+    d = {'images': opts.images, 'atlas': opts.atlas}
+    fp = '%s.json' % op.splitext(opts.output)[0]
+    json.dump(d, open(fp, 'w'))
