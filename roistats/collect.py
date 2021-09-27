0145514ec49be1228ebbef639714e4a9c30cf265
xgrg <goperto@gmail.com>
2021-09-27 18:37:33 +0200

    FIX tests

--- roistats/collect.py
+++ roistats/collect.py
@@ -1,46 +1,49 @@
-import numpy as np
 import nibabel as nib
-from glob import glob
 import pandas as pd
 from joblib import Parallel, delayed
 import logging as log
+import numpy as np
+
 
 def _roistats_from_map(map_fp, atlas, func=np.mean):
-     m = np.array(nib.load(map_fp).dataobj)
-     assert(m.shape == atlas.shape)
-     n_labels = list(np.unique(atlas))
-     label_values = dict([(label, func(m[atlas==label])) for label in n_labels])
-     return label_values
+    m = np.array(nib.load(map_fp).dataobj)
+    assert(m.shape == atlas.shape)
+    n_labels = list(np.unique(atlas))
+    label_values = dict([(label, func(m[atlas == label]))
+                         for label in n_labels])
+    return label_values
+
 
 def roistats_from_maps(maps_fp, atlas_fp, subjects=None,
-	   func=np.mean, n_jobs=8):
-     if not subjects is None and len(subjects) != len(maps_fp):
-         log.error('Images (%s) and subjects (%s) mismatch in size'
-                 %(len(maps_fp), len(subjects)))
-         return None
+                       func=np.mean, n_jobs=8):
+    if subjects is not None and len(subjects) != len(maps_fp):
+        log.error('Images (%s) and subjects (%s) mismatch in size'
+                  % (len(maps_fp), len(subjects)))
+        return None
 
-     log.info('Collecting ROI stats on %s images (atlas: %s)'
-             %(len(maps_fp), atlas_fp))
+    log.info('Collecting ROI stats on %s images (atlas: %s)'
+             % (len(maps_fp), atlas_fp))
 
-     # Load atlas and count ROIs
-     atlas_im = nib.load(atlas_fp)
-     atlas = np.array(atlas_im.dataobj)
+    # Load atlas and count ROIs
+    atlas_im = nib.load(atlas_fp)
+    atlas = np.array(atlas_im.dataobj)
 
-     roi_labels = list(np.unique(atlas))
+    roi_labels = list(np.unique(atlas))
 
-     # Run it on every image
-     df = Parallel(n_jobs=n_jobs, verbose=1)(\
-         delayed(_roistats_from_map)(maps_fp[i], atlas, func)\
-         for i in range(0, len(maps_fp)))
+    # Run it on every image
+    df = Parallel(n_jobs=n_jobs, verbose=1)(
+        delayed(_roistats_from_map)(maps_fp[i], atlas, func)
+        for i in range(0, len(maps_fp)))
 
-     # Name rows and columns and return the DataFrame
-     columns = [int(e) for e in roi_labels]
-     res = pd.DataFrame(df, columns=columns)
+    # Name rows and columns and return the DataFrame
+    columns = [int(e) for e in roi_labels]
+    res = pd.DataFrame(df, columns=columns)
 
-     res['subject'] = range(0, len(maps_fp)) if subjects is None else subjects
-     res = res.set_index('subject')
+    res['subject'] = range(0, len(maps_fp)) if subjects is None else subjects
+    res = res.set_index('subject')
 
-     return res
+    return res
+
 
 def roistats_from_map(map_fp, atlas_fp,	func=np.mean):
     return roistats_from_maps([map_fp], atlas_fp, func=func, )
